421c22f7f957a97621b438bfe58584026565cd6e
ciyuxu <YOUR@EMAIL.com>
2017-09-13 13:26:26 +0200

    Update all todo counters

---
+++ main.js
@@ -0,0 +1,18 @@
+var languageName;
+languageName = "Javascript";
+document.write(languageName);
+var age;
+age = 32;
+document.write(age);
+var a;
+a = 6;
+var b;
+b = 12;
+document.write(a+b);
+var firstName="Wouter", lastName="de Vos", age=25;
+document.write(firstName + " " + lastName + " " + "is" +" " + age);
+var languageName = "Ruby";
+
+if (languageName !== "JavaScript") {
+  document.write("I love JavaScript");
+}
